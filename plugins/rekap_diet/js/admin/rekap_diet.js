cdb2293ab2cd65920ec5f756a1dddc453873d516
erliana <anaerliana2025@gmail.com>
2023-03-03 11:58:25 +0800

    tambah item diet

--- plugins/rekap_diet/js/admin/rekap_diet.js
+++ plugins/rekap_diet/js/admin/rekap_diet.js
@@ -34,13 +34,7 @@ $("#index").on('click', '#tutupform', function(){
   $("#tutupform").attr("id", "bukaform");
 });
 
-// // ketika inputbox kd_diet diklik
-// $("#form").on("click","#kd_diet", function(event){
-//   var kd_diet_baru = $("#kd_diet_baru").val();
-//   $("#kd_diet").val(kd_diet_baru);
-// });
-
-$("#form").on("click", ".kd_diet", function(event){
+$("#index").on('click', '#bukaform', function(){
   var baseURL = mlite.url + '/' + mlite.admin;
   event.preventDefault();
   var url = baseURL + '/rekap_diet/kodediet?t=' + mlite.token;
@@ -51,6 +45,17 @@ $("#form").on("click", ".kd_diet", function(event){
   });
 });
 
+// $("#form").on("click", ".kd_diet", function(event){
+//   var baseURL = mlite.url + '/' + mlite.admin;
+//   event.preventDefault();
+//   var url = baseURL + '/rekap_diet/kodediet?t=' + mlite.token;
+//   $.get(url ,function(data) {
+//     // tampilkan data
+//     //console.log(data);
+//     $("#kd_diet").val(data);
+//   });
+// });
+
 $("#form").on("click",".batal",function(event){
     event.preventDefault();
     $("#form").hide();
@@ -58,45 +63,6 @@ $("#form").on("click",".batal",function(event){
     $("#tutupform").attr("kd_diet", "bukaform");
     $("input:text[name=kd_diet]").val("");
     $("input:text[name=nama_diet]").val("");
-});
-
-$("#form").on("click", "#simpan_itemdiet", function(event){
-  var baseURL = mlite.url + '/' + mlite.admin;
-  event.preventDefault();
-  var kd_diet         = $('input:text[name=kd_diet]').val();
-  var nama_diet       = $('input:text[name=nama_diet]').val();
-  console.log({
-    kd_diet, nama_diet
-  });
-
-  var url = baseURL + '/rekap_diet/itemdietsave?t=' + mlite.token;
-  $.post(url, {
-    kd_diet : kd_diet,
-    nama_diet: nama_diet,
-  }, function(data) {
-    console.log(data);
-
-     var url = baseURL + '/rekap_diet/itemdiet?t=' + mlite.token;
-    $.get(url, function(data) {
-      // tampilkan data
-     // console.log(data);
-      $("#index").html(data).show();
-
-    });
-      $('input:text[name=kd_diet]').val("");
-      $('input:text[name=nama_diet]').val("");
-      $('#notif').html("<div class=\"alert alert-success alert-dismissible fade in\" role=\"alert\" style=\"border-radius:0px;margin-top:-15px;\">"+
-      "Data Item Diet telah disimpan!"+
-      "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">&times;</button>"+
-      "</div>").show();
-    // } else {
-    //   $('#notif').html("<div class=\"alert alert-danger alert-dismissible fade in\" role=\"alert\" style=\"border-radius:0px;margin-top:-15px;\">"+
-    //   "Data Item Diet gagal disimpan!"+
-    //   "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">&times;</button>"+
-    //   "</div>").show();
-    // }
-  });
-   //alert("coba lagi");
 });
 
 $("#display").on("click",".edititem",function(event){
@@ -107,38 +73,10 @@ $("#display").on("click",".edititem",function(event){
     var nama_diet = $(this).attr("data-nama_diet");
     var kd_diet = $(this).attr("data-kd_diet");
 
+
+    // $('input:hidden[name=edit]').val('1');
     $("input:text[name=nama_diet]").val(nama_diet);
     $("input:text[name=kd_diet]").val(kd_diet);
+
     // document.getElementById("kd_diet").value = kd_diet;
 });
-
-// // ketika tombol hapus ditekan
-// $("#display").on("click",".hapus_itemdiet", function(event){
-// var baseURL = mlite.url + '/' + mlite.admin;
-// event.preventDefault();
-// var url = baseURL + '/rekap_diet/hapusitemdiet?t=' + mlite.token;
-// var kd_diet = $(this).attr("data-kd_diet");
-
-// // tampilkan dialog konfirmasi
-// bootbox.confirm("Apakah Anda yakin ingin menghapus data ini?", function(result){
-//   // ketika ditekan tombol ok
-//   if (result){
-//     // mengirimkan perintah penghapusan
-//     $.post(url, {
-//       kd_diet: kd_diet,
-//     } ,function(data) {
-//       var url = baseURL + '/rekap_diet/itemdiet?t=' + mlite.token;
-//       $.get(url, {kd_diet : kd_diet,
-//       }, function(data) {
-//         // tampilkan data
-//         $("#manage").html(data).show();
-//       });
-//      $('input:text[name=nama_diet]').val("");
-//       $('#notif').html("<div class=\"alert alert-danger alert-dismissible fade in\" role=\"alert\" style=\"border-radius:0px;margin-top:-15px;\">"+
-//       "Data Item Diet telah dihapus!"+
-//       "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">&times;</button>"+
-//       "</div>").show();
-//     });
-//   }
-// });
-// });
